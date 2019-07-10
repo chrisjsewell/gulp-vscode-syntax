3901e8dca6c6403aa3eebfdeecddf8a651621c04
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-10 07:52:04 +0100

    added snippet completion for output formats

--- convert_help_file.py
+++ convert_help_file.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 
 with open("help_file.txt") as handle:
     lines = handle.readlines()
@@ -27,5 +28,16 @@ for i, line in enumerate(lines):
             "description": "\n".join(description)
         }
 
+with open("help_file_output_types.yaml") as handle:
+    output_data = yaml.load(handle)
+
+for name, descript in output_data.items():
+    ident = "output-" + name
+    snippets[ident] = {
+        "prefix": ident,
+        "body": "output {} ${{0:filename}}".format(name),
+        "description": descript
+    }
+
 with open("snippets/gulp-snippets.code-snippets", "w") as handle:
-    json.dump(snippets, handle)
+    json.dump(snippets, handle, indent=2)
